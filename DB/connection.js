9ae63bd5474e1f7c2d18c270d0738dac901f502e
alan mathew <alanmathew25.5.1999@gmail.com>
2024-01-26 10:00:32 +0530

    imrw sever

--- DB/connection.js
+++ DB/connection.js
@@ -3,7 +3,7 @@
 
 const mongoose = require('mongoose')
 
-const connectionstring = process.env.DATABASE
+const connectionstring = process.env.DATABASE.toString()
 
 mongoose.connect(connectionstring).then((res) =>{
     console.log("mongodb connnected successfully");
